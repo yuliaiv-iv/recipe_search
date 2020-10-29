90d74da26addbaa010183b21fb4d65e294e3b0aa
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2020-10-29 13:47:55 +1000

    Updates

---
+++ precache-manifest.0dcf8b8552076336024a8e65543c0055.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ebc65a8ec85f9a601ea6248aab0031b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e6a3eefc5c6cc77ccf",
+    "url": "/static/css/main.86e0d4d8.chunk.css"
+  },
+  {
+    "revision": "391437c6d4323dfab6a4",
+    "url": "/static/js/2.9696f42c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9696f42c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72e6a3eefc5c6cc77ccf",
+    "url": "/static/js/main.17a2b17f.chunk.js"
+  },
+  {
+    "revision": "9690ffb68a1e9c5c1f49",
+    "url": "/static/js/runtime-main.b3ef1ce7.js"
+  },
+  {
+    "revision": "fa8bc24a9d0961de868250f206b02970",
+    "url": "/static/media/logo.fa8bc24a.svg"
+  },
+  {
+    "revision": "d04bbc2703be6be0d7b139db85b00c09",
+    "url": "/static/media/next.d04bbc27.svg"
+  }
+]);
